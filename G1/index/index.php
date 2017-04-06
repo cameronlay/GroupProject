68f0b9b57e69bcca7b2323a402821c7b0c943284
Sunguk Ham <ham2959@gmail.com>
2017-04-06 12:46:20 -0700

    incomplete index slideshow

--- G1/index/index.php
+++ G1/index/index.php
@@ -143,20 +143,24 @@
         var slideIndex = 0;
 showSlides();
 
-function showSlides() {
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";  
     }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1}    
+    n++;
+    if (n> slides.length) {n = 1}    
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    slides[n-1].style.display = "block";  
+    dots[n-1].className += " active";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
