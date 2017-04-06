126add9367a92d7ceabf8a606a953f4570aad3fc
Sunguk Ham <ham2959@gmail.com>
2017-04-06 12:21:12 -0700

    index slideshow

--- G1/index/index.php
+++ G1/index/index.php
@@ -114,7 +114,12 @@
                 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
                 <a class="next" onclick="plusSlides(1)">&#10095;</a>
                 <br>
-            </div><!-- 
+            </div>
+            <div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div><!-- 
             <img src="../images/monitortransparent_mini.jpg" alt="Main Pictures">
 
             <img src="../images/keyboardtransparent_mini.jpg" alt="Main Pictures">
@@ -135,36 +140,25 @@
 //       4700);
 // });
 
-        var slideIndex = 1;
-        
-        showSlides(slideIndex);
+        var slideIndex = 0;
+showSlides();
 
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
-        }
-
-        function currentSlide(n) {
-            showSlides(slideIndex = n);
-        }
-
-        function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            
-            if (n > slides.length) {
-                slideIndex = 1
-            } 
-            
-            if (n < 1) {
-                slideIndex = slides.length
-            }
-            
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none"; 
-            }
-            
-            slides[slideIndex-1].style.display = "block";
-        }
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
     </script>
 
